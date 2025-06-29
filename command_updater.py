35b2cae03d5f88e473d495228b7c87443af7e4b6
Amir-Litvak <amirlitvak@gmail.com>
2025-06-29 22:50:52 +0300

    changing course

---
+++ command_updater.py
@@ -0,0 +1,17 @@
+import os #path
+from pathlib import Path
+
+import repo_parser
+import repo_importer
+
+class CommandUpdater:
+    def __init__(self):
+        self._curr_dir = os.path.dirname(os.path.abspath(__file__))
+        self._commands_json = f"{self._curr_dir}/commands.json"
+        if not os.path.isfile(self._commands_json):
+            self._commands_json = open(f"{self._curr_dir}/commands.json", "x")
+
+    def import_repo(self, repo_url, repo_name):
+        repo_importer.RepoImporter(self._curr_dir, repo_url, repo_name)
+
+                
---
+++ main.py
@@ -0,0 +1,5 @@
+import command_updater
+
+if __name__ == "__main__":
+    CU = command_updater.CommandUpdater()
+    CU.import_repo("https://github.com/LOLBAS-Project/LOLBAS", "LOLBAS-project") #import and update LOLBAS repo
