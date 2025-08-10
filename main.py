2cdccbeb78e4ddf60fcbffd22128262a6e1d608c
DrorM <drorm@infosecu.net>
2025-08-10 15:11:13 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import command_updater
-
-if __name__ == "__main__":
-    CU = command_updater.CommandUpdater()
-    CU.import_repo("https://github.com/LOLBAS-Project/LOLBAS", "LOLBAS-project") #import and update LOLBAS repo
-    CU.parse_repo("LOLBAS-project")
